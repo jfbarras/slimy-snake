86210159fc9e9314081d2c649165d4532d35b413
jfbarras <jfbarras@gmail.com>
2018-05-19 15:49:36 -0400

    Move canvas to own file

--- bot.user.js
+++ bot.user.js
@@ -8,37 +8,13 @@ The MIT License (MIT)
 // ==UserScript==
 // @name         Slither.io Snake Whisperer
 // @namespace    https://github.com/jfbarras/slimy-snake
-// @version      0.0.3
+// @version      0.1.1
 // @description  Slither.io Snake Whisperer
 // @author       J.-F. Barras
 // @match        http://slither.io/
+// @require      src/canvas.js
 // @grant        none
 // ==/UserScript==
-
-var canvas = window.canvas = (function(window) {
-    return {
-        // Adjusts zoom in response to mouse wheel.
-        setZoom: function(e) {
-            if (window.gsc) {
-                window.gsc *= Math.pow(0.9, e.wheelDelta / -120 || e.detail / 2 || 0);
-                window.desired_gsc = window.gsc;
-            }
-        },
-
-        // Restores zoom to the default value.
-        resetZoom: function() {
-            window.gsc = 0.9;
-            window.desired_gsc = 0.9;
-        },
-
-        // Sets zoom to desired zoom.
-        maintainZoom: function() {
-            if (window.desired_gsc !== undefined) {
-                window.gsc = window.desired_gsc;
-            }
-        }
-    };
-})(window);
 
 var userInterface = window.userInterface = (function(window, document) {
     // Saves original slither.io functions so they can be modified, or reenabled.
