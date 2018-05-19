c4e1f9d7047de0c0ac827c4c6923d81274d1ea09
jfbarras <jfbarras@gmail.com>
2018-05-19 15:50:32 -0400

    Update canvas.js

--- src/canvas.js
+++ src/canvas.js
@@ -1,1 +1,24 @@
+var canvas = window.canvas = (function(window) {
+    return {
+        // Adjusts zoom in response to mouse wheel.
+        setZoom: function(e) {
+            if (window.gsc) {
+                window.gsc *= Math.pow(0.9, e.wheelDelta / -120 || e.detail / 2 || 0);
+                window.desired_gsc = window.gsc;
+            }
+        },
 
+        // Restores zoom to the default value.
+        resetZoom: function() {
+            window.gsc = 0.9;
+            window.desired_gsc = 0.9;
+        },
+
+        // Sets zoom to desired zoom.
+        maintainZoom: function() {
+            if (window.desired_gsc !== undefined) {
+                window.gsc = window.desired_gsc;
+            }
+        }
+    };
+})(window);
