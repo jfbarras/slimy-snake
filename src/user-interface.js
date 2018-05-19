bdcb8792b1a3e81d81e6172c3beb42c590e75d21
jfbarras <jfbarras@gmail.com>
2018-05-19 15:59:46 -0400

    Update user-interface.js

--- src/user-interface.js
+++ src/user-interface.js
@@ -1,1 +1,31 @@
+var userInterface = window.userInterface = (function(window, document) {
+    // Saves original slither.io functions so they can be modified, or reenabled.
+    var original_keydown = document.onkeydown;
 
+    return {
+        onkeydown: function(e) {
+            // Triggers original slither.io onkeydown function.
+            original_keydown(e);
+            if (window.playing) {
+                // Allows letter 'Z' to reset zoom.
+                if (e.keyCode === 90) {
+                    canvas.resetZoom();
+                }
+            }
+        },
+
+        oefTimer: function() {
+            var start = Date.now();
+            canvas.maintainZoom();
+
+            if (!window.no_raf) {
+                window.raf(userInterface.oefTimer);
+            } else {
+                const bot_opt_targetFps = 60;
+                setTimeout(userInterface.oefTimer,
+                    (1000 / bot_opt_targetFps) - (Date.now() - start));
+            }
+        }
+
+    };
+})(window, document);
