9791b75273eb1f4934d5a3104ca8b083f617bf1a
jfbarras <jfbarras@gmail.com>
2018-05-19 13:18:22 -0400

    Add canvas.resetZoom
    Brings in userInterface.onkeydown too

--- bot.user.js
+++ bot.user.js
@@ -8,7 +8,7 @@ The MIT License (MIT)
 // ==UserScript==
 // @name         Slither.io Snake Whisperer
 // @namespace    https://github.com/jfbarras/slimy-snake
-// @version      0.0.1
+// @version      0.0.2
 // @description  Slither.io Snake Whisperer
 // @author       J.-F. Barras
 // @match        http://slither.io/
@@ -23,12 +23,37 @@ var canvas = window.canvas = (function(window) {
                 window.gsc *= Math.pow(0.9, e.wheelDelta / -120 || e.detail / 2 || 0);
                 window.desired_gsc = window.gsc;
             }
+        },
+
+        // Restores zoom to the default value.
+        resetZoom: function() {
+            window.gsc = 0.9;
+            window.desired_gsc = 0.9;
         }
     };
 })(window);
 
+var userInterface = window.userInterface = (function(window, document) {
+    // Saves original slither.io functions so they can be modified, or reenabled.
+    var original_keydown = document.onkeydown;
+
+    return {
+        onkeydown: function(e) {
+            // Triggers original slither.io onkeydown function.
+            original_keydown(e);
+            if (window.playing) {
+                // Allows letter 'Z' to reset zoom.
+                if (e.keyCode === 90) {
+                    canvas.resetZoom();
+                }
+            }
+        }
+    };
+})(window, document);
+
 // Main
 (function(window, document) {
+    document.onkeydown = userInterface.onkeydown;
 
     // Listens for mouse wheel scroll.
     document.body.addEventListener('mousewheel', canvas.setZoom);
