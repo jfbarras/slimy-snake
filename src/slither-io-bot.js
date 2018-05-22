ab5fc5bfc0920d3ee9c7ac869d7425edf14b8608
jfbarras <jfbarras@gmail.com>
2018-05-22 16:26:42 -0400

    bot.speedMult and drawSideCircles and call

--- src/slither-io-bot.js
+++ src/slither-io-bot.js
@@ -15,12 +15,50 @@ var bot = window.bot = (function(window) {
                 window.fmlts[sct] - 1) - 5);
         },
 
+        drawSideCircles: function() {
+          var sidecircle;
+    			var lnp = window.snake.lnp;
+
+          //inner-right
+          sidecircle = canvas.circle(
+              lnp.xx - ((lnp.yy + bot.sin * bot.snakeWidth) - lnp.yy),
+              lnp.yy + ((lnp.xx + bot.cos * bot.snakeWidth) - lnp.xx),
+              bot.snakeRadius * bot.speedMult
+          );
+          canvas.drawCircle(sidecircle, 'darkred', false);
+
+          //inner-left
+          sidecircle = canvas.circle(
+              lnp.xx + ((lnp.yy + bot.sin * bot.snakeWidth) - lnp.yy),
+              lnp.yy - ((lnp.xx + bot.cos * bot.snakeWidth) - lnp.xx),
+              bot.snakeRadius * bot.speedMult
+          );
+          canvas.drawCircle(sidecircle, 'darkred', false);
+
+          //outer-right
+          sidecircle = canvas.circle(
+              lnp.xx - ((lnp.yy + bot.sin * bot.snakeWidth) - lnp.yy),
+              lnp.yy + ((lnp.xx + bot.cos * bot.snakeWidth) - lnp.xx),
+              3 * bot.snakeRadius * bot.speedMult
+          );
+          canvas.drawCircle(sidecircle, 'darkred', false);
+
+          //outer-left
+          sidecircle = canvas.circle(
+              lnp.xx + ((lnp.yy + bot.sin * bot.snakeWidth) - lnp.yy),
+              lnp.yy - ((lnp.xx + bot.cos * bot.snakeWidth) - lnp.xx),
+              3 * bot.snakeRadius * bot.speedMult
+          );
+          canvas.drawCircle(sidecircle, 'darkred', false);
+        },
+
         every: function() {
             bot.cos = Math.cos(window.snake.ang);
             bot.sin = Math.sin(window.snake.ang);
             bot.xx = window.snake.xx + window.snake.fx;
             bot.yy = window.snake.yy + window.snake.fy;
 
+            bot.speedMult = window.snake.sp / 5.78; //bot.opt.speedBase;
             bot.snakeWidth = bot.getSnakeWidth();
             bot.snakeRadius = bot.snakeWidth / 2;
             bot.snakeLength = bot.getSnakeLength();
@@ -29,6 +67,8 @@ var bot = window.bot = (function(window) {
                 // coral food collection sector
                 canvas.drawAngle(window.snake.ehang - Math.PI/4, window.snake.ehang + Math.PI/4,
                     3 * bot.snakeRadius, 'coral', false);
+                // dark red circles depict turn snakeRadius
+                bot.drawSideCircles();
             }
         }
     };
