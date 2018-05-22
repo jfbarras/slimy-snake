9e58db1eefc9fcb1269ee3b6fcad796d15cf9258
jfbarras <jfbarras@gmail.com>
2018-05-21 22:54:14 -0400

    add width, length & every

--- src/slither-io-bot.js
+++ src/slither-io-bot.js
@@ -2,6 +2,28 @@
 var bot = window.bot = (function(window) {
     return {
         state: 'init',
-        scores: []
+        scores: [],
+
+        getSnakeWidth: function(sc) {
+            if (sc === undefined) sc = window.snake.sc;
+            return Math.round(sc * 29.0);
+        },
+
+        getSnakeLength: function(sct) {
+            if (sct === undefined) sct = window.snake.sct;
+            return Math.floor(15 * (window.fpsls[sct] + window.snake.fam /
+                window.fmlts[sct] - 1) - 5);
+        },
+
+        every: function() {
+            bot.cos = Math.cos(window.snake.ang);
+            bot.sin = Math.sin(window.snake.ang);
+            bot.xx = window.snake.xx + window.snake.fx;
+            bot.yy = window.snake.yy + window.snake.fy;
+
+            bot.snakeWidth = bot.getSnakeWidth();
+            bot.snakeRadius = bot.snakeWidth / 2;
+            bot.snakeLength = bot.getSnakeLength();
+        }
     };
 })(window);
